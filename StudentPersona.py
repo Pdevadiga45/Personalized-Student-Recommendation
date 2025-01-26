9e8eff6df27b52f02443ae3f5eab3a4567c658c6
Pdevadiga45 <pdevadiga451@gmail.com>
2025-01-26 20:17:01 +0530

    Update StudentPersona.py

--- StudentPersona.py
+++ StudentPersona.py
@@ -1,8 +1,5 @@
-import numpy as np
-from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import json
-from collections import defaultdict
 
 with open(r'D:\internassignment\HistoricalData.json', 'r') as file:
     data = json.load(file)
@@ -19,15 +16,16 @@ for entry in data:
     user_found = next((user for user in user_data if user["user_id"] == user_id), None)
     
     if user_found:
+        
         user_found["total_speed"] += speed
         user_found["total_accuracy"] += (correct_answers / total_answered) * 100  
         user_found["count"] += 1
     else:
-        # Add new user to the list
+       
         user_data.append({
             "user_id": user_id,
             "total_speed": speed,
-            "total_accuracy": (correct_answers / total_answered) * 100, 
+            "total_accuracy": (correct_answers / total_answered) * 100,  
             "count": 1
         })
 
@@ -50,8 +48,6 @@ fake_data = [
     {"user_id": "userid4", "avg_speed": "35", "avg_accuracy": "30%"},
 ]
 
-    # Appending sample_data from more users for better visualization
-
 for entry in fake_data:
     user_id = entry["user_id"]
     avg_speed = int(entry["avg_speed"])     
@@ -63,7 +59,9 @@ for entry in fake_data:
         "avg_accuracy": avg_accuracy
     })
 
+# Print the assigned persona for each user
 for averages in user_averages:
+    # Determine persona based on average speed and accuracy
     avg_speed = averages["avg_speed"]
     avg_accuracy = averages["avg_accuracy"]
     
@@ -81,7 +79,6 @@ for averages in user_averages:
 # Plot each user's average speed and accuracy
 for averages in user_averages:
     plt.scatter(averages['avg_speed'], averages['avg_accuracy'], label=averages['user_id'])
-
 plt.axvline(50, color='red', linestyle='--', label='Speed = 50')
 plt.axhline(50, color='green', linestyle='--', label='Accuracy = 50')
 plt.xlim(0, 100)
